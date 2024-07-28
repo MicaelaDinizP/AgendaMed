6428993ad2b5d26f7467718398742b9332fb4d63
Micaela <micaelahimura@hotmail.com>
2024-07-28 19:23:48 -0300

    refactor: move a lógica de implementação de menus para MenuFragment

--- app/src/main/java/devandroid/micaela/tcc_agendamed/view/UsuarioActivity.java
+++ app/src/main/java/devandroid/micaela/tcc_agendamed/view/UsuarioActivity.java
@@ -21,13 +21,11 @@ import java.util.List;
 
 import devandroid.micaela.tcc_agendamed.R;
 import devandroid.micaela.tcc_agendamed.controller.UsuarioController;
+import devandroid.micaela.tcc_agendamed.model.MenuFragment;
 import devandroid.micaela.tcc_agendamed.model.Usuario;
 
 public class UsuarioActivity extends AppCompatActivity {
-
     private Button btnCriarUsuario;
-    private Toolbar toolbarTop;
-    private Toolbar toolbarBottom;
     private List<Usuario> listaUsuarios;
     private TableLayout tabelaUsuarios;
     private UsuarioController usuarioController;
@@ -37,12 +35,12 @@ public class UsuarioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuario);
         this.usuarioController = new UsuarioController(UsuarioActivity.this);
-        //criação da toolbar
-        toolbarTop = findViewById(R.id.toolbar_top);
-        toolbarBottom = findViewById(R.id.toolbar_bottom);
 
-        setSupportActionBar(toolbarTop);
-        setupBottomToolbar();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(new MenuFragment(), "MENU_FRAGMENT")
+                    .commit();
+        }
 
         this.tabelaUsuarios = findViewById(R.id.tableUsuarios);
 
@@ -112,75 +110,5 @@ public class UsuarioActivity extends AppCompatActivity {
         row.addView(botaoEditar);
 
         this.tabelaUsuarios.addView(row);
-    }
-
-    //FUNCTIONS DE MENU
-    private void atualizarUsuarioLogado(Usuario novoUsuarioLogado){
-        //pego o usuario atual
-        //removo o background color dele
-        //adiciono o novoUsuario na variavel estatica
-        //troco a cor do usuario novo pra verde
-        //deixo o botão anterior ativo
-        //deixo o botão atual inativo
-        MainActivity.USUARIO_LOGADO = novoUsuarioLogado;
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_top, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        MenuItem activeUserName = menu.findItem(R.id.menu_active_user_name);
-        activeUserName.setTitle(MainActivity.USUARIO_LOGADO.getNome());
-        //Aqui define o nome do usuário ativo. Caso seja alterado usar o recreate()
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.menu_active_user) {
-            Toast.makeText(this, "Menu ACTIVE USER clicked", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        if (id == R.id.menu_settings) {
-            Toast.makeText(this, "Menu SETTINGS clicked", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        if (id == R.id.menu_active_user_name) {
-            Toast.makeText(this, "Menu ACTIVE USER NAME clicked", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void setupBottomToolbar() {
-        toolbarBottom.inflateMenu(R.menu.menu_bottom);
-        toolbarBottom.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                int id = item.getItemId();
-                if (id == R.id.menu_users) {
-                    Toast.makeText(UsuarioActivity.this, "Bottom Toolbar USERS clicked", Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-                if (id == R.id.menu_alarms) {
-                    Toast.makeText(UsuarioActivity.this, "Bottom Toolbar ALARMS clicked", Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-                if (id == R.id.menu_medicines) {
-                    Toast.makeText(UsuarioActivity.this, "Bottom Toolbar MEDICINES clicked", Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-                if (id == R.id.menu_manual) {
-                    Toast.makeText(UsuarioActivity.this, "Bottom Toolbar MANUAL clicked", Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-                return false;
-            }
-        });
     }
 }
