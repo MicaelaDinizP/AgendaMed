1ea2bc2a2698745c2d2711c9403ce034fbd378fa
Micaela <micaelahimura@hotmail.com>
2024-08-17 01:28:42 -0300

    feat: adiciona a tela de cadastro de medicamento

---
+++ app/src/main/java/devandroid/micaela/tcc_agendamed/view/CadastrarMedicamentoActivity.java
@@ -0,0 +1,21 @@
+package devandroid.micaela.tcc_agendamed.view;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import devandroid.micaela.tcc_agendamed.R;
+
+public class CadastrarMedicamentoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_medicamento);
+
+    }
+
+}
