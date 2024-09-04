29edfd36aa1a0a41d105486468fcbb3946598378
Micaela <micaelahimura@hotmail.com>
2024-09-03 23:53:58 -0300

    feat: adiciona uma exception específica para ColecaoUsuarios

---
+++ app/src/main/java/devandroid/micaela/tcc_agendamed/exception/ColecaoUsuariosException.java
@@ -0,0 +1,10 @@
+package devandroid.micaela.tcc_agendamed.exception;
+
+public class ColecaoUsuariosException extends RuntimeException{
+    public ColecaoUsuariosException(Throwable cause) {
+        super(cause);
+    }
+    public ColecaoUsuariosException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
