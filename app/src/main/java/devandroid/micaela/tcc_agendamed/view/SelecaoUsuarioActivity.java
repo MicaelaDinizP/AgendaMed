f5f9cdb3f74951e41198b36a9ef850f1a196b445
Micaela <micaelahimura@hotmail.com>
2024-09-15 19:50:14 -0300

    feat: implementa as classes para a splash screen e tela de seleção de usuários

---
+++ app/src/main/java/devandroid/micaela/tcc_agendamed/view/SelecaoUsuarioActivity.java
@@ -0,0 +1,65 @@
+package devandroid.micaela.tcc_agendamed.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+import devandroid.micaela.tcc_agendamed.R;
+import devandroid.micaela.tcc_agendamed.controller.UsuarioController;
+import devandroid.micaela.tcc_agendamed.model.Usuario;
+
+public class SelecaoUsuarioActivity extends AppCompatActivity {
+    private UsuarioController usuarioController;
+    private TableLayout tabelaLoginUsuario;
+    private List<Usuario> listaUsuarios;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selecao_usuario);
+
+        this.usuarioController = new UsuarioController(this);
+        this.tabelaLoginUsuario = findViewById(R.id.tableLoginUsuario);
+        this.usuarioController.abrirConexao();
+        this.listaUsuarios = this.usuarioController.obterTodos();
+        if(this.listaUsuarios.isEmpty()){
+            Toast.makeText(SelecaoUsuarioActivity.this, "Erro ao carregar o aplicativo." , Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        this.desenharTabela();
+    }
+    private void desenharTabela() {
+        for (Usuario usuario : this.listaUsuarios) {
+            desenharLinha(usuario);
+        }
+    }
+    private  void desenharLinha(Usuario usuario){
+        TableRow row = new TableRow(this);
+        row.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+
+        Button botaoLogarUsuario = new Button(this);
+        botaoLogarUsuario.setText(usuario.getNome());
+        botaoLogarUsuario.setPadding(8, 8, 8, 8);
+        botaoLogarUsuario.setGravity(Gravity.CENTER);
+        botaoLogarUsuario.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1f));
+        botaoLogarUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.USUARIO_LOGADO = usuario;
+                Intent intent = new Intent(SelecaoUsuarioActivity.this, UsuarioActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        row.addView(botaoLogarUsuario);
+        this.tabelaLoginUsuario.addView(row);
+    }
+}
---
+++ app/src/main/java/devandroid/micaela/tcc_agendamed/view/SplashScreenActivity.java
@@ -0,0 +1,39 @@
+package devandroid.micaela.tcc_agendamed.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+import devandroid.micaela.tcc_agendamed.R;
+import devandroid.micaela.tcc_agendamed.controller.UsuarioController;
+import devandroid.micaela.tcc_agendamed.model.Usuario;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private static final int SPLASH_DURATION = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        UsuarioController usuarioController = new UsuarioController(this);
+        usuarioController.abrirConexao();
+        List<Usuario> listaUsuarios = usuarioController.obterTodos();
+
+        if(listaUsuarios.isEmpty()){
+            Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+        }else{
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(SplashScreenActivity.this, SelecaoUsuarioActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, SPLASH_DURATION);
+        }
+    }
+}
