909402202719a6e265a7649c738c7097faa7fa6e
Micaela <micaelahimura@hotmail.com>
2024-12-10 21:46:09 -0300

    feat: adiciona estrutura para a classe de gerenciamento dos alarmes

--- app/src/main/java/devandroid/micaela/tcc_agendamed/model/GerenciadorAlarme.java
+++ app/src/main/java/devandroid/micaela/tcc_agendamed/model/GerenciadorAlarme.java
@@ -1,2 +1,20 @@
-package devandroid.micaela.tcc_agendamed.model;public class GerenciadorAlarme {
+package devandroid.micaela.tcc_agendamed.model;
+
+public class  GerenciadorAlarme {
+    public static boolean checarAlarmesAtivo(boolean $statusPredefinido){
+            return $statusPredefinido;
+    }
+     public static boolean criarAlarme(boolean $statusPredefinido){
+         return $statusPredefinido;
+     }
+
+    public static boolean excluirAlarme(boolean $statusPredefinido){
+        return $statusPredefinido;
+    }
+    public static boolean desativarAlarme(boolean $statusPredefinido){
+        return $statusPredefinido;
+    }
+    public static boolean ativarAlarme(boolean $statusPredefinido){
+        return $statusPredefinido;
+    }
 }
